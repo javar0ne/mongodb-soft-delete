7fcf0407237bebf0f41fca57c71bf40d0cf1f45f
Giuseppe Iavarone <giuseppe.iavarone@alten.it>
2023-07-14 15:32:11 +0200

    fix: soft delete
    add equals and hashcode

--- src/main/java/com/giavarone/mongodbsoftdelete/model/SoftDelete.java
+++ src/main/java/com/giavarone/mongodbsoftdelete/model/SoftDelete.java
@@ -1,5 +1,6 @@
 package com.giavarone.mongodbsoftdelete.model;
 
+import lombok.EqualsAndHashCode;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -7,6 +8,7 @@ import java.time.LocalDateTime;
 
 @Getter
 @Setter
+@EqualsAndHashCode
 public abstract class SoftDelete {
     private LocalDateTime deletedAt;
 }
